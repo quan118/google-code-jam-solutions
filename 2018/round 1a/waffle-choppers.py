04d6a272bafadb1b008ded364b0148fcadfd9df7
Quan Nguyen <quan.nguyen@niteco.se>
2019-04-12 07:41:19 +0700

    Finish implementation for waffle choppers problem

--- 2018/round 1a/waffle-choppers.py
+++ 2018/round 1a/waffle-choppers.py
@@ -1,4 +1,3 @@
-
 def solve(waffle, R, C, H, V, cid):
   nchip = 0
   for r in range(R):
@@ -7,12 +6,12 @@ def solve(waffle, R, C, H, V, cid):
         nchip += 1
   npieces = (V+1)*(H+1)
 
-  if nchip % npieces != 0 or nchip % H != 0 or nchip % V != 0:
+  if nchip % npieces != 0 or nchip % (H+1) != 0 or nchip % (V+1) != 0:
     print('Case #%d: IMPOSSIBLE' % (cid+1))
     return
   
   possible = True
-  nchip_in_a_row = nchip // H
+  nchip_in_a_row = nchip // (H + 1)
   cnt = 0
   rows = []
   cols = []
@@ -28,7 +27,7 @@ def solve(waffle, R, C, H, V, cid):
       break
   
   if possible:
-    nchip_in_a_col = nchip // V
+    nchip_in_a_col = nchip // (V+1)
     for c in range(C):
       for r in range(R):
         if waffle[r][c] == '@':
@@ -40,7 +39,26 @@ def solve(waffle, R, C, H, V, cid):
         possible = False
         break
   
-  
+  r0 = 0
+  c0 = 0
+  cnt = 0
+  nchip_per_piece = nchip // npieces
+
+  for r1 in rows:
+    for c1 in cols:
+      cnt = 0
+      for r in range(r0, r1+1):
+        for c in range(c0, c1+1):
+          if waffle[r][c] == '@':
+            cnt += 1
+      c0 = c1 + 1
+      if cnt != nchip_per_piece:
+        possible = False
+        break
+    if possible == False:
+      break
+    r0 = r1 + 1
+    c0 = 0
 
   if possible:
     print('Case #%d: POSSIBLE' % (cid+1))
