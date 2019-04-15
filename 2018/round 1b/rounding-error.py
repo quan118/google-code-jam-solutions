7de7399cea9bcddee4c674055490fc4c269e842a
Quan Nguyen <quan.nguyen@niteco.se>
2019-04-15 23:40:37 +0700

    Implementing solution for rounding error

--- 2018/round 1b/rounding-error.py
+++ 2018/round 1b/rounding-error.py
@@ -1,19 +1,62 @@
+from functools import reduce
+
 def solve(N, L, C, cid):
+  need = [0] * (N+1)
+  marker = 0
+  best_choice = 0
+  for i in range(1,N+1):
+    tmp = i * 1000 // N
+    d = tmp % 10
+    if d == 0 or d >= 5:
+      if best_choice <= 0 and d >= 5:
+        best_choice = i
+      need[i] = 0
+      if marker > 0:
+        for j in range(marker, i):
+          need[j] = i - j
+        marker = 0
+    elif marker <= 0:
+      marker = i
+  # print(need)
+
+  # print('best_choice:%d' % best_choice)
+  unknown = N - reduce(lambda x, y: x + y, C)
+  # print('unknown:%d' % unknown)
+
   C1 = []
   for c in C:
-    tmp = c * 1000 // N
-    last_digit = tmp % 10
-    if last_digit >= 5:
-      continue
-    else:
-      C1.append((5-last_digit)*N//10)
+    if best_choice > 0 and need[c] < best_choice:
+      C1.append(need[c])
+
+  # for i in range(n_best_choice):
+  #   C1.append(best_choice)
+  # print(C1)
   C1.sort()
-  tmp = 1000 // N
-  last_digit = tmp % 10
-  best_choice = 1
-  if last_digit < 5:
-    best_choice += (5-last_digit)*N//10
+  acc = reduce(lambda x, y: x + y, map(lambda c: round(c*100/N), C))
+  # print('acc:%d' % acc)
+  for c in C1:
+    unknown -= c
+    if unknown < 0:
+      unknown += c
+      break
+    if c > 0:
+      acc += 1 + round(c*100/N)
   
+  n_best_choice = 0
+  if best_choice > 0 and unknown > 0:
+    n_best_choice = unknown // best_choice
+
+  for i in range(n_best_choice):
+    acc += round(best_choice*100/N)
+    unknown -= best_choice
+    if unknown < 0:
+      unknown += best_choice
+      break
+  
+  if unknown > 0:
+    acc += round(unknown*100/N)
+
+  print('Case #%d: %d' % (cid+1, acc))
 
 if __name__=="__main__":
   T = int(input())
