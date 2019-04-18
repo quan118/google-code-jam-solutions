14ecfc27f3c9ee9d37172098fbe79979414cabf1
Quan Nguyen <quan.nguyen@niteco.se>
2019-04-18 11:21:37 +0700

    Finish implementation for rounding error problem

--- 2018/round 1b/rounding-error.py
+++ 2018/round 1b/rounding-error.py
@@ -1,11 +1,8 @@
 from functools import reduce
-import decimal
-from decimal import Decimal
-
-decimal.getcontext().rounding = decimal.ROUND_HALF_UP
 
 def round1(num):
-  return int(Decimal(num).quantize(Decimal('1')))
+  d = int(num * 10) % 10 
+  return int(num) if d < 5 else int(num + 1)
 
 def solve(N, L, C, cid):
   need = [0] * (N+1)
@@ -14,9 +11,8 @@ def solve(N, L, C, cid):
   for i in range(1,N+1):
     tmp = i * 1000 // N
     d = tmp % 10
-    if d == 0 or d >= 5:
-      if best_choice <= 0 and d >= 5:
-        best_choice = i
+    if d >= 5:
+      best_choice = i if best_choice <= 0 and d >= 5 else best_choice
       need[i] = 0
       if marker > 0:
         for j in range(marker, i):
@@ -24,43 +20,27 @@ def solve(N, L, C, cid):
         marker = 0
     elif marker <= 0:
       marker = i
-  # print(need)
 
-  # print('best_choice:%d' % best_choice)
   unknown = N - reduce(lambda x, y: x + y, C)
-  # print('unknown:%d' % unknown)
 
-  C1 = []
-  for c in C:
-    if best_choice > 0 and need[c] < best_choice:
-      C1.append(need[c])
 
-  # for i in range(n_best_choice):
-  #   C1.append(best_choice)
-  # print(C1)
-  C1.sort()
-  acc = reduce(lambda x, y: x + y, map(lambda c: round1(c*100/N), C))
-  # print('acc:%d' % acc)
-  for c in C1:
-    unknown -= c
-    if unknown < 0:
-      unknown += c
-      break
-    if c > 0:
-      acc += 1 + round1(c*100/N)
-      # print('acc:%d' % acc)
-  # print('acc2:%d' % acc)
+  C1 = [(need[c],c) for c in C]
+  C1.sort(key=lambda c1: c1[0])
+
+  acc = 0
+  for c1 in C1:
+    if c1[0] > best_choice or unknown < c1[0] or c1[0] <= 0:
+      acc += round1(c1[1]*100/N)
+    else:
+      unknown -= c1[0]
+      acc += round1((c1[0]+c1[1])*100/N)
+
   n_best_choice = 0
   if best_choice > 0 and unknown > 0:
     n_best_choice = unknown // best_choice
+  acc += n_best_choice * round1(best_choice*100/N)
+  unknown -= n_best_choice * best_choice
 
-  for i in range(n_best_choice):
-    acc += round1(best_choice*100/N)
-    unknown -= best_choice
-    if unknown < 0:
-      unknown += best_choice
-      break
-  
   if unknown > 0:
     acc += round1(unknown*100/N)
 
